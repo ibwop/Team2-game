dfbf25f6f14097112b8c126ead4fbe74cc885e45
Ben Jarvis <84350300+benjarvis25@users.noreply.github.com>
2024-10-21 14:17:35 +0100

    Add files via upload

---
+++ items.py
@@ -0,0 +1,20 @@
+from file_manager import *
+
+# items_descriptions.txt FORMAT:
+# name // description
+
+class item:
+    def __init__(self, n, d): # l, dam):
+        self.name = n
+        self.description = d
+        #self.location_found = l
+        #self.damage = dam
+
+def initialise_items():
+    data = read_file("items_descriptions.txt")
+    items = {}
+    for i in data:
+        items[i[0]] = item(i[0], i[1]) # i[2], i[3])
+    return items
+
+items_list = initialise_items() # dict of all item objects, indexed by name
