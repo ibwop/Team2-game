37be7ab790b9fc433b7b5a25b40567319dbfd221
Callum Acourt <callumacourtt@gmail.com>
2024-10-21 13:14:37 +0100

    fixed items

--- items.py
+++ items.py
@@ -4,15 +4,21 @@ from file_manager import *
 # name // description
 
 class item:
-    def __init__(self, n, d):
+    def __init__(self, n, d, l, x):
         self.name = n
         self.description = d
+        self.location = l
+        self.damage = x
 
 def initialise_items():
     data = read_file("items_descriptions.txt")
     items = {}
     for i in data:
-        items[i[0]] = item(i[0], i[1])
+        try:
+            name, description, location, damage = i
+            items[name] = item(name, description, location, damage)
+        except ValueError:
+            print(f"Skipping line due to unpacking error: {i}")
     return items
 
 items_list = initialise_items() # dict of all item objects, indexed by name
